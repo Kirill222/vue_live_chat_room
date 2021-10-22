26d81e2e598914969863c1ce357a227d31c7607b
johnconnor <zkmd2012@inbox.ru>
2021-10-22 14:04:43 +0500

    2 Firestore connection setup, timestamps setup

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBUfySHa3xbDFosFWBXHlR1o4E7zVcUOSw",
+    authDomain: "vue-projects-8b935.firebaseapp.com",
+    projectId: "vue-projects-8b935",
+    storageBucket: "vue-projects-8b935.appspot.com",
+    messagingSenderId: "76226660680",
+    appId: "1:76226660680:web:0ca07b84354f97ad91cb7d"
+  };
+
+  //init firebase
+  firebase.initializeApp(firebaseConfig)
+  //init firestore service
+  const projectFirestore = firebase.firestore()
+  //in order to use timestamps
+  const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+  export {projectFirestore, timestamp}
